2758a2c0dd769651e8f677fe0ba2627205b6394b
finallbossl <nguyenluan.28122003@gmail.com>
2026-05-29 18:55:55 +0700

    first commit

---
+++ src/main/java/ai_quynhon_tourism_management/tourism/TourismApplication.java
@@ -0,0 +1,13 @@
+package ai_quynhon_tourism_management.tourism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourismApplication.class, args);
+	}
+
+}
